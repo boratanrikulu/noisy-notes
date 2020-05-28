386418af35d60453f02bad5830c3d23c187300ac
boratanrikulu <boratanrikulu@gmail.com>
2020-05-28 15:34:40 +0300

    Add json responses for welcome, signup and login endpoints

--- controllers/user.go
+++ controllers/user.go
@@ -1,44 +1,72 @@
 package controllers
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 
 	"github.com/boratanrikulu/noisy-notes/models"
 )
 
 // SignUp creates users
-//
 // Form must contains "username" and "password".
 func SignUp(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+
 	username := r.PostFormValue("username")
 	password := r.PostFormValue("password")
 
 	err := models.SignUp(username, password)
 	if err != nil {
+		// Return 403. There is an issue with creating account.
 		w.WriteHeader(http.StatusForbidden)
-		fmt.Fprint(w, err)
+
+		_ = json.NewEncoder(w).Encode(struct {
+			Error string
+		}{
+			Error: err.Error(),
+		})
 		return
 	}
 
+	// Return 202. Account is created.
 	w.WriteHeader(http.StatusAccepted)
-	fmt.Fprint(w, "Account is created.")
+	_ = json.NewEncoder(w).Encode(struct {
+		Message string
+	}{
+		Message: "Account is created.",
+	})
 }
 
 // Login creates session and returns a token.
+// Form must contains "username" and "password".
 func Login(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+
 	username := r.PostFormValue("username")
 	password := r.PostFormValue("password")
 
 	token, err := models.Login(username, password)
 	if err != nil {
+		// Return 403. There is an issue with login.
 		w.WriteHeader(http.StatusForbidden)
-		fmt.Fprint(w, err)
+
+		_ = json.NewEncoder(w).Encode(struct {
+			Error string
+		}{
+			Error: err.Error(),
+		})
 		return
 	}
 
+	// Return 202. Login is successful.
 	w.WriteHeader(http.StatusAccepted)
-	fmt.Fprintf(w, "Login is successful. Token: %v", token)
+	_ = json.NewEncoder(w).Encode(struct {
+		Message string
+		Token   string
+	}{
+		Message: "Login is successful.",
+		Token:   token,
+	})
 }
 
 // Delete deletes the account.
--- controllers/welcome.go
+++ controllers/welcome.go
@@ -1,13 +1,23 @@
 package controllers
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
+type pageResponse struct {
+	Error   string
+	Message string
+}
+
 // WelcomeGet just returns a hello message.
 func Welcome(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	response := "{ \"response\":\"Hello World.\" }"
-	fmt.Fprint(w, response)
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+
+	res := pageResponse{
+		Message: "Hey!",
+	}
+
+	w.WriteHeader(http.StatusOK)
+	_ = json.NewEncoder(w).Encode(res)
 }
